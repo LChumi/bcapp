8582fa5cc7d4e1f4b676663fca8bef655bc1dfc2
LChumi <112025391+LChumi@users.noreply.github.com>
2024-07-11 19:19:34 -0500

    feat:(adding routes)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'banco',
+        children:[
+            {
+                path:'home',
+                title: 'Home',
+                loadComponent: ()=> import('./layouts/home/home.component')
+            },
+            {
+                path:'login',
+                title: 'Login',
+                loadComponent: ()=> import('./layouts/login/login.component')
+            },
+            {
+                path:'acces',
+                title:'Control de Acceso',
+                loadComponent: ()=> import('./layouts/acces-control/acces-control.component')
+            },
+            {
+                path:'dashboard',
+                title:'Dashboard',
+                loadComponent: ()=> import('./layouts/dashboard/dashboard.component')
+            },
+            {path:'', redirectTo:'home',pathMatch:'full'},
+            {path:'**', redirectTo:'home',pathMatch:'full'},
+        ]
+    },
+    {path:'',redirectTo:'banco/home',pathMatch:'full'},
+    {path:'**',redirectTo:'banco/home',pathMatch:'full'}
+];
